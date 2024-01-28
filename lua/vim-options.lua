59a194f6e38e8952a0a7c930965da51b52808617
eric <Ectsang@users.noreply.github.com>
2024-01-28 14:34:17 +0800

    easy escape and python comment toggle

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,6 +7,9 @@ vim.cmd("set number relativenumber")
 vim.g.mapleader = " "
 
 vim.keymap.set("n", ";", ":")
+vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-s>", ":w<CR>")
 vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>")
+vim.keymap.set("n", "m/", "<Esc>_i#<Space><Esc>l")
+vim.keymap.set("n", "n/", "<Esc>_xx<Esc>")
