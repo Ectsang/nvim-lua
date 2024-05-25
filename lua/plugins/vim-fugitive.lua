e8215323d1af18ca89936e7423e3c288dc3bad71
0xSmartCrypto <0xSmartCrypto@gmail.com>
2024-05-25 23:43:15 +0800

    add git push as well

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,6 +1,9 @@
 return {
   {
     "tpope/vim-fugitive",
-    vim.keymap('n', '<leader>v', ':Gvdiffsplit'),
+    vim.keymap.set('n', '<leader>v', ':Gvdiffsplit<CR>'),
+    vim.keymap.set('n', '<leader>ga', ':Git add .<CR>'),
+    vim.keymap.set('n', '<leader>gc', ':Git commit<CR>'),
+    vim.keymap.set('n', '<leader>gp', ':Git push<CR>'),
   },
 }
