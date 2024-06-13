a5e5b3ac8190deeae12b811cb8d544c5da93a521
e <e@e.e>
2024-06-13 15:07:28 +0800

    relative time in blamer

--- lua/plugins/blamer.lua
+++ lua/plugins/blamer.lua
@@ -3,7 +3,8 @@ return {
 		"APZelos/blamer.nvim",
 		config = function()
 			vim.g.blamer_enabled = true
-      vim.g.blamer_delay = 500
+      vim.g.blamer_delay = 300
+      vim.g.blamer_relative_time = 1
 		end,
 	},
 }
